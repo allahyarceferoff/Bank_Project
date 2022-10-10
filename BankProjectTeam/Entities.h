64a372cb8a7fc43f33780ba3ee01b059d5e8d399
Ceferov Allahyar Xaliq <Cefer_hg45@itstep.edu.az>
2022-10-10 13:57:00 +0400

    Struct add in Entities.h

--- BankProjectTeam/Entities.h
+++ BankProjectTeam/Entities.h
@@ -1,1 +1,32 @@
 #pragma once
+
+
+struct Debtor {
+	char* name;
+	char* surname;
+	int id;
+	int debt;
+	int payment;
+
+
+
+};
+
+
+
+struct Log {
+	int id;
+	char* content;
+	const char* datetime = __DATE__;
+};
+
+
+
+
+struct Bank {
+	char* bankname;
+	int debtor_count;
+	int totaldebt;
+	int totalbudget;
+};
+
