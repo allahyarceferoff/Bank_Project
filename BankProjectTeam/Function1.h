a9671d385c4a636e67f58a9e80a3a300cb9f75b4
Ceferov Allahyar Xaliq <Cefer_hg45@itstep.edu.az>
2022-10-10 14:36:31 +0400

    I add GiveCredit function

--- BankProjectTeam/Function1.h
+++ BankProjectTeam/Function1.h
@@ -2,7 +2,8 @@
 #include<iostream>
 using namespace std;
 #include"Entities.h"
-Debtor** debtors = new Debtor * [] {//Allahyar
+#include"Function2.h"
+Debtor** debtors = new Debtor * [] {
 	new Debtor{ new char[] {"John"},new char[] {"Johnlu"},123, 100000, 7000 },
 		new Debtor{ new char[] {"Harry"},new char[] {"Harryli"},234, 120000, 2000 },
 		new Debtor{ new char[] {"Jack"},new char[] {"Jones"},345, 1430000, 10000 },
@@ -11,14 +12,14 @@ Debtor** debtors = new Debtor * [] {//Allahyar
 
 
 
-Bank** bank = new Bank * [] {//Allahyar
+Bank** bank = new Bank * [] {
 	new Bank{ new char [] {"Kapital Bank"}, 3, totaldebt, totalbud },
 };
 
 
 
 
-void ShowMenu() {              //Allahyar
+void ShowMenu() {             
 	cout << "============================" << endl;
 	cout << "Show all debtors [1] : " << endl;
 	cout << "Show any debtor's payment [2] : " << endl;
@@ -26,4 +27,22 @@ void ShowMenu() {              //Allahyar
 	cout << "Give credit any debtor [4] : " << endl;
 	cout << "See total budget of bank [5] : " << endl;
 	cout << "See total debtors' debt : " << endl;
+}
+
+
+
+void GiveCredit(Debtor** debtors) {   
+	ShowDebtors(debtors);
+	cout << "Enter debtor's id : ";
+	int id = 0;
+	cin >> id;
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (debtors[i]->debt <= 10000) {
+			cout << "Enter credite cost : ";
+			int cost = 0;
+			cin >> cost;
+			debtors[i]->debt += cost;
+		}
+	}
 }
