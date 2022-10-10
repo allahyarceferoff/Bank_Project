e1733022a62c00ff1aade121607a34f937bbe58c
Baxshiyev Eynal Eynulla <Baxsh_cw07@itstep.edu.az>
2022-10-10 14:27:46 +0400

    I added ShowDebtors function

--- BankProjectTeam/Function2.h
+++ BankProjectTeam/Function2.h
@@ -19,3 +19,19 @@ void GetTotalBudget(Debtor** debtors) {
 
 	totalbud = 10000000 - totaldebt;
 }
+
+
+void ShowDebtors(Debtor** debtors) {   
+	system("cls");
+	cout << "DEBTORS INFORMATION" << endl << endl;
+	for (size_t i = 0; i < 3; i++)
+	{
+		cout << "Debtor's name : " << debtors[i]->name << endl;
+		cout << "Debtor's surname : " << debtors[i]->surname << endl;
+		cout << "Debtor's id : " << debtors[i]->id << endl;
+		cout << "Debtor's debt : " << debtors[i]->debt << endl;
+		cout << "Debtor's payment : " << debtors[i]->payment << endl;
+		cout << endl;
+	}
+}
+
